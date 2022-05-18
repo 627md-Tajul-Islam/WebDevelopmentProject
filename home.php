7ae4fda2e16d14c9a0422e4b2fd083dcdaccad22
Md. Tajul Islam <tajul.azazil@gmail.com>
2022-05-18 23:14:29 +0600

    header and footer done

---
+++ home.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Travel</title>
+
+<!-- Swiper CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+<!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+<!-- Custom Css -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<!-- Main Coding starts here -->
+<body>
+
+<!-- header section starts  -->
+<section class="header">
+<!-- Title starts here  -->
+   <a href="home.php" class="logo">Travel</a>
+<!-- Title ends here  -->
+<!-- menu starts here  -->
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+<!-- menu ends here  -->
+<!-- Menu Toggle Icon starts here  -->
+   <div id="menu-btn" class="fas fa-bars"></div>
+<!-- Menu Toggle Icon ends here  -->
+</section>
+<!-- header section ends -->
+
+
+<!-- footer starts  -->
+<section class="footer">
+<!-- footer menu starts here  -->
+   <div class="box-container">
+<!-- Quick link menu starts here  -->
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+<!-- Quick link menu ends here  -->
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>mr. web designer</span> | all rights reserved! </div>
+<!-- footer menu ends here  -->
+</section>
+<!-- footer ends -->
+
+
+
+
+<!-- Swiper JS-->
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+<!-- Custom JS -->
+    <script src="js/script.js"></script>
+</body>
+<!-- Main Coding ends here -->
+</html>
